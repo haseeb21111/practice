64208f488cbb7af4cd2a5125f543523f7c4952f3
haseeb21111 <20055911@mydbs.ie>
2025-01-27 01:55:42 +0000

    nana

--- java.js
+++ java.js
@@ -1,6 +1,15 @@
+// Function that will be called when the button is clicked
 let doIt = () => {
-    x = document.getElementById("bw").value;  // Get the value of the input with id "bw"
-    alert(x);  // Show the baggage weight entered in an alert
-    alert("done");  // Show a "done" message in an alert
-
+    // Get the baggage weight entered by the user
+    let weight = document.getElementById("bw").value;
+    
+    // Check if the weight is greater than 15kg
+    if (weight > 15) {
+        // Show an alert if the bag is overweight
+        alert("Your baggage is overweight! Please reduce the weight.");
+    } else {
+        // Show an alert if the weight is within the limit
+        alert("Your baggage is within the weight limit.");
+    }
 }
+
