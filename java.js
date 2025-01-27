58f196b82589d05094a77f97b07cecea8b7be287
haseeb21111 <20055911@mydbs.ie>
2025-01-27 01:46:24 +0000

    gh

--- java.js
+++ java.js
@@ -2,4 +2,6 @@ let doIt = () => {
     x = document.getElementById("bw").value;  // Get the value of the input with id "bw"
     alert(x);  // Show the baggage weight entered in an alert
     alert("done");  // Show a "done" message in an alert
+
+    
 }
